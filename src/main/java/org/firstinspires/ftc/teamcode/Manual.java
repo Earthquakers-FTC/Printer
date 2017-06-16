93e7d32943bc890f9e519e1462dc6ae838447434
Sibren Talens <sibrentalens@gmail.com>
2017-06-16 21:46:21 +0200

    Make this less readable

--- src/main/java/org/firstinspires/ftc/teamcode/Manual.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Manual.java
@@ -3,10 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
-import java.util.List;
-
+/**
+ * A manual OpMode to control the robot with a handheld controller
+ */
 @TeleOp(name="TeleOp: Manual", group="Linear Opmode")
 public class Manual extends LinearOpMode {
 
@@ -22,15 +22,9 @@ public class Manual extends LinearOpMode {
 
         while(opModeIsActive()) {
 
-            if(gamepad1.left_trigger > 0.0)
-                power = -Math.pow(gamepad1.left_trigger, 2.0);
-            else
-                power = Math.pow(gamepad1.right_trigger, 2.0);
+            power = gamepad1.left_trigger > 0.0 ? Math.pow(gamepad1.left_trigger, 2.0) : -Math.pow(gamepad1.left_trigger, 2.0);
 
-            if(this.gamepad1.left_stick_x > 0.0)
-                steering = Math.pow(gamepad1.left_stick_x, 2.0);
-            else
-                steering = -Math.pow(gamepad1.left_stick_x, 2.0);
+            steering = gamepad1.left_stick_x > 0.0 ? Math.pow(gamepad1.left_stick_x, 2.0) : -Math.pow(gamepad1.left_stick_x, 2.0);
 
             hardwareMap.dcMotor.get("fr").setPower(normalize(power + steering)); // Front Right
             hardwareMap.dcMotor.get("rr").setPower(normalize(power + steering)); // Rear Right
